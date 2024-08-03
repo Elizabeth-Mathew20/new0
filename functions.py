5ddfd9447787571e61f3f09379c9f461c914e246
elizabeth mathew20 <elizabethmatheweliza5@gmail.com>
2024-08-03 12:12:38 +0530

    first commit

---
+++ functions.py
@@ -0,0 +1,141 @@
+# def family():
+#     print("helo")
+# family()    
+
+
+
+# def add():
+#     a=10
+#     b=20
+#     print(a+b)
+# add()
+
+
+
+
+# a=10
+# b=5
+# def add():
+#     print(a+b)
+# add()    
+
+
+
+# def sum(x,y):
+#     print(x+y)
+# sum(2,5)
+
+
+
+
+
+# def sum(*x):
+#     print(x[3])
+# sum(12,3,5,7)    
+
+
+
+
+
+# def sum(x,y,z):
+#     print(y)
+# sum(x=1,y=3,z=5)    
+
+
+
+
+# def sum(**x):
+#     print(x["a"])
+# sum(a=1,d=5,t=8)    
+
+
+
+
+
+
+
+
+
+# # default arg
+
+# def num(country="india"):
+#     print(country)
+# # num("ireland")
+# num()
+
+
+
+
+# s=[1,2,3,4,5,6]
+# def num(x):
+#     for i in s:
+#         print(i)
+# num(s)    
+
+
+
+
+
+
+# # return stmnt
+
+# # def num(x):
+# #     return 10+x
+# # print(num(2))
+
+
+
+# def num():
+#     pass
+
+
+
+
+# # s=lambda a:a+10
+# # print(s(10))
+
+
+# a=lambda x,y:x+y
+# print(a(10,20))
+
+
+
+
+
+# map
+# l=["apple","banana","mango","orange"]
+# a=list(map(lambda p:str.upper(p),l))
+# print(a)
+
+
+
+# a=["a","d","r","u"]
+# b=list(map(lambda c:str.upper(c),a))
+# print(b)
+
+
+# s=["R","H","U","O"]
+# u=list(map(lambda t:str.lower(t),s))
+# print(u)
+
+
+
+# filter
+# n=[1,2,3,4,5]
+# a=list(filter(lambda p:p>3,n))
+# print(a)
+
+
+
+
+# number= int(input("Enter a number: "))
+
+# if number % 2 == 0:
+#     print("The number is even")
+# else:
+#     print("The number is odd")
+
+
+
+
+
